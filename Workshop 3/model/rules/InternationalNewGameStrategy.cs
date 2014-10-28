f8f63e3cf846a0df92c05126c6d689a1fa96d2f0
vl222cu <vl222cu@student.lnu.se>
2014-10-28 02:58:09 +0100

    Missade att ta bort lite kod tidigare

--- Workshop 3/model/rules/InternationalNewGameStrategy.cs
+++ Workshop 3/model/rules/InternationalNewGameStrategy.cs
@@ -10,7 +10,6 @@ namespace BlackJack.model.rules
 
         public bool NewGame(Deck a_deck, Dealer a_dealer, Player a_player)
         {
-            Card c;
 
             a_dealer.Deal(a_player, true);
             a_dealer.Deal(a_dealer, true);
